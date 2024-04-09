731cafaae49d4d6f63c06daa6ba2156c84615a0b
laukik-24 <laukiklanje24@gmail.com>
2024-04-10 00:56:57 +0530

    updated

--- app.js
+++ app.js
@@ -3,7 +3,28 @@ const app = express();
 const port = 8080;
 const mongoose = require("mongoose");
 
-app.get('/', (req, res) => res.send('Hello World!'));
+
+main()
+    .then(() => {
+        console.log("successful connection");
+    })
+    .catch(err => console.log(err));                            
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/watsapp'); 
+}
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+});
+
+
+
+
+
+
+
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}!`);
 });
